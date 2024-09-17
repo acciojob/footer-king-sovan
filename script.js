02baba63ea6babe601b99b76029cf4b961b358b9
king-sovan <156076876+king-sovan@users.noreply.github.com>
2024-09-17 13:30:13 +0530

    created a submission

--- script.js
+++
@@ -1,4 +0,0 @@
-cy.visit("http://localhost:3000/");
-cy.wait(500); // Add a small wait to allow page to load
-cy.get(".media-icons").should("have.css", "display", "flex");
-cy.get(".media-icons a").should("have.length", 5);
